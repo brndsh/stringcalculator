4e4b2cf6a9a82429146ff0a69e815c9f96d9e045
Brndsh <bryndise14@hr.is>
2015-10-16 22:11:13 +0000

    Daemi 4 med test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -13,23 +13,24 @@ public class Calculator {
 		}*/
 		else if(text.contains("//"))
 		{
-			return sum(splitNumbers(removeBeginning(text)));
+			return sum(removeBeginning(text));
 		}
 		else if(text.contains(",") || text.contains("\n") || text.contains(";")){
 			return sum(splitNumbers(text));
 		}
-
 		else
 			return toInt(text);
 	}
 
-	private static String removeBeginning(String text){
+	private static String[] removeBeginning(String numbers){
+		String storeDelim = numbers.substring(2,3);	//Store the delimeter
+		numbers = numbers.replaceAll("\n", "");				//Replace newline with empty string
+		numbers = numbers.substring(3);		//Removing first 3 letters
+		numbers = numbers.replaceAll(storeDelim, ";");		//Replace every delimeter with ";"
 		
-		String storeText = text.substring(5);
-		text = storeText;
-		return text;
+		return splitNumbers(numbers);
 	}
-	
+
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
